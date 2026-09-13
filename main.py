90f59f91bbc3f81437fb0ec63760f5d458e25e16
faranesfan-commits <faranesfan@gmail.com>
2026-09-13 11:08:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+expenses = []
 
+def add_expense():
+    title = input("Expense name: ")
+    amount = float(input("Amount: "))
+    category = input("Category: ")
+
+    expense = {
+        "title": title,
+        "amount": amount,
+        "category": category
+    }
+
+    expenses.append(expense)
+    print("Expense added.")
+
+def show_expenses():
+    for expense in expenses:
+        print(expense)
+
+while True:
+    print("\n1. Add expense")
+    print("2. Show expenses")
+    print("3. Exit")
+
+    choice = input("Choose: ")
+
+    if choice == "1":
+        add_expense()
+    elif choice == "2":
+        show_expenses()
+    elif choice == "3":
+        break
+    else:
+        print("Invalid option")
